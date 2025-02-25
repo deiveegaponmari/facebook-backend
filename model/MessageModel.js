24f222bf57093a4c899aacabeadf2cf0b820ac6b
ponmarimdeiveega@gmail.com <ponmarimdeiveega@example.com>
2025-02-25 14:39:03 +0530

    changes added by message modal

--- model/MessageModel.js
+++ model/MessageModel.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
    senderId:{
-        type:Number
+        type:String,
+        required:true
     } ,
     recipientId:{
-        type:Number
+        type:String,
+        required:true
     },
     text: {
         type: String,
